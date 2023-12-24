f7532d99262a4690da077979ab1753d01dcd8e3e
Victor Stamoff <transtrike@gmail.com>
2023-12-24 22:17:27 +0200

    Initial setup

---
+++ set-user-xdg-env-variables.sh
@@ -0,0 +1,19 @@
+# User specific configuration variables
+REPO_FOLDER=$(dirname $0)/..
+
+# Home folder
+XDG_DATA_HOME=$REPO_FOLDER/local/share
+# XDG_DATA_DIRS=$REPO_FOLDER/data
+
+# Config folder
+XDG_CONFIG_HOME=$REPO_FOLDER/config
+#XDG_CONFIG_DIRS=$REPO_FOLDER/config
+
+# State folder
+XDG_STATE_HOME=$REPO_FOLDER/local/state
+
+# Cashe folder 
+XDG_CACHE_HOME=$REPO_FOLDER/local/cache
+
+# Runtime folder
+# XDG_RUNTIME_DIR=
