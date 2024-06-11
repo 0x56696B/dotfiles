d8f56bb05067deaf9028d3f3a71c38e840967243
Viktor Stamoff <transtrike@gmail.com>
2024-06-11 12:24:56 +0300

    feat: added to neovim: terraform, git integration and harpoon

--- roles/neovim/files/lua/plugins/lang/git.lua
+++ roles/neovim/files/lua/plugins/lang/git.lua
@@ -1,13 +1,3 @@
-local treesitter = { 'git_config', 'git_rebase', 'gitcommit', 'gitignore' }
-
 return {
-  -- Syntax highlighting
-  {
-    'nvim-treesitter/nvim-treesitter',
-    opts = function(_, opts)
-      if type(opts.ensure_installed) == 'table' then
-        vim.list_extend(opts.ensure_installed, treesitter)
-      end
-    end,
-  },
+  { import = 'lazyvim.plugins.extras.lang.git' },
 }
