af76de91b2194aeda5a3c7be43cd148ce74057ae
Viktor Stamoff <transtrike@gmail.com>
2024-07-10 13:10:02 +0300

    fix: tmux new session script

--- roles/tmux/files/scripts/tmux-new-dir-session.sh
+++ roles/tmux/files/scripts/tmux-new-dir-session.sh
@@ -6,24 +6,24 @@ CHOSEN_DIR=$(fd \
   --hidden \
   --follow \
   --exclude .git \
-  --maxdepth 2 \
-  | fzf --border \
-  --margin 1 \
-  --padding 1 \
-  --info inline \
-  --layout reverse \
-  --header $'Select new TMUX session working directory' \
-  --prompt 'Dir> ' \
-  --bind 'ctrl-p:up,ctrl-n:down,J:down,K:up' \
-  --walker=dir \
-  --tmux center)
+  --maxdepth 1 |
+  fzf --border \
+    --margin 1 \
+    --padding 1 \
+    --info inline \
+    --layout reverse \
+    --header $'Select new TMUX session working directory' \
+    --prompt 'Dir> ' \
+    --bind 'ctrl-p:up,ctrl-n:down,J:down,K:up' \
+    --walker=dir \
+    --tmux center)
+
 if [ -z "$CHOSEN_DIR" ]; then
   exit 0
 fi
 
-
 # Create the new session
-tmux new-session -d -s ${CHOSEN_DIR%/} -c ${CHOSEN_DIR%/} -F ${CHOSEN_DIR%/} -P
+tmux new-session -d -s ${CHOSEN_DIR%/} -c "$(pwd)/${CHOSEN_DIR%/}" #-P -F ${CHOSEN_DIR%/}
 
 # Attach to the new session
 tmux switch-client -t ${CHOSEN_DIR%/}
