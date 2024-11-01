67308096048ecc9bae6c00804542e3923496e482
Viktor Stamoff <transtrike@gmail.com>
2024-11-01 22:39:40 +0200

    feat: configure bufferline

---
+++ roles/neovim/files/lua/plugins/editor/bufferline.lua
@@ -0,0 +1,12 @@
+return {
+  {
+    'akinsho/bufferline.nvim',
+    opts = {
+      options = {
+        -- Disable tabpages
+        mode = 'buffers',
+        always_show_bufferline = false,
+      },
+    },
+  },
+}
