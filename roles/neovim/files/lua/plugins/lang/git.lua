ddf5527c5156845e5ad2bf6021f7c6329cb175be
Viktor Stamoff <transtrike@gmail.com>
2024-11-14 11:53:29 +0200

    fix: remove unused git plugin

--- roles/neovim/files/lua/plugins/lang/git.lua
+++ roles/neovim/files/lua/plugins/lang/git.lua
@@ -1,18 +1,5 @@
-vim.api.nvim_create_autocmd('User', {
-  pattern = 'GitConflictDetected',
-  callback = function()
-    vim.notify('Conflict detected in '..vim.fn.expand('<afile>'))
-    vim.keymap.set('n', 'cww', function()
-      engage.conflict_buster()
-      create_buffer_local_mappings()
-    end)
-  end
-})
-
 return {
   { import = 'lazyvim.plugins.extras.lang.git' },
-
-  { 'akinsho/git-conflict.nvim', version = '*', config = true },
 
   {
     'lewis6991/gitsigns.nvim',
