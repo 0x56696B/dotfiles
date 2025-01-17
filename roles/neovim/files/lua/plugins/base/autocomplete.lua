d05e713f6c4b821705103e85922e156e6a8ed4e7
Viktor Stamoff <transtrike@gmail.com>
2025-01-17 22:16:56 +0200

    feat(nvim): add blink experimentally

--- roles/neovim/files/lua/plugins/base/autocomplete.lua
+++ roles/neovim/files/lua/plugins/base/autocomplete.lua
@@ -1,19 +1,58 @@
-return {
-  {
-    'hrsh7th/nvim-cmp',
-    opts = function(_, opts)
-      local cmp = require 'cmp'
-
-      local config = {
-        region_check_events = 'CursorHold,InsertLeave',
-        delete_check_events = 'TextChanged,InsertEnter',
-        mapping = cmp.mapping.preset.insert {
-          ['<C-y>'] = cmp.mapping.confirm { select = true },
-          ['<CR>'] = nil,
+if vim.g.enable_blink then
+  return {
+    {
+      "saghen/blink.cmp",
+      opts = {
+        appearance = {
+          use_nvim_cmp_as_default = true, -- Let it stay until blink works as expected
+        },
+        completion = {
+          documentation = {
+            auto_show_delay_ms = 20,
+          },
+        },
+        sources = {
+          providers = {
+            lsp = {
+              min_keyword_length = 0,
+              async = true,
+            },
+          }
+        },
+        -- experimental signature help support
+        signature = { enabled = true },
+        keymap = {
+          preset = "default",
         },
       }
+    }
+  }
+else
+  return {
+    { import = 'lazyvim.plugins.extras.coding.nvim-cmp' },
 
-      return vim.tbl_deep_extend('force', opts, config)
-    end,
-  },
-}
+    {
+      'hrsh7th/nvim-cmp',
+      enabled = true,
+      opts = function(_, opts)
+        local cmp = require 'cmp'
+
+        local config = {
+          region_check_events = 'CursorHold,InsertLeave',
+          delete_check_events = 'TextChanged,InsertEnter',
+          completion = {
+            keyword_length = 1,
+            max_item_count = 7,
+          },
+          mapping = cmp.mapping.preset.insert {
+            ['<C-y>'] = cmp.mapping.confirm { select = true },
+            ["<C-Space>"] = cmp.mapping.complete(),
+            ['<CR>'] = nil,
+          },
+        }
+
+        return vim.tbl_deep_extend('force', opts, config)
+      end,
+    },
+  }
+end
