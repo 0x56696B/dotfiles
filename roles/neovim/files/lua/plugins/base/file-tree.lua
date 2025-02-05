c279fa29bb03ef7194f2b71823e554cbd79d78f2
Viktor Stamoff <transtrike@gmail.com>
2025-02-05 13:08:03 +0200

    feat: replace neo-tree with oil.nvim

--- roles/neovim/files/lua/plugins/base/file-tree.lua
+++ roles/neovim/files/lua/plugins/base/file-tree.lua
@@ -1,46 +1,93 @@
+-- vim.api.nvim_set_hl(0, "OilGitIgnored", { link = "Comment" })
+
 return {
+  { "nvim-neo-tree/neo-tree.nvim", enabled = false },
+
   {
-    'nvim-neo-tree/neo-tree.nvim',
-    lazy = true,
-    version = '*',
+    "stevearc/oil.nvim",
+    lazy = false,
     dependencies = {
-      'nvim-lua/plenary.nvim',
-      'nvim-tree/nvim-web-devicons',
-      'MunifTanjim/nui.nvim',
-      '3rd/image.nvim'
+      { "echasnovski/mini.icons", opts = {} },
+      { "nvim-tree/nvim-web-devicons" }
     },
+    keys = {
+      {
+        "<leader>E",
+        function()
+          if vim.bo.filetype == "oil" then
+            vim.cmd("bd")
+          else
+          require("oil").open(LazyVim.root())
+          end
+        end,
+
+        desc = "Open Root directory",
+        mode = "n"
+      },
+      {
+        "<leader>e",
+        function()
+          if vim.bo.filetype == "oil" then
+            vim.cmd("bd")
+          else
+            require('oil').open()
+          end
+        end,
+        desc = "Toggle Oil in Parent directory",
+        mode = "n",
+      },
+
+    },
+    ---@module "oil"
+    ---@type oil.SetupOpts
     opts = {
-      follow_current_file = {
-        enabled = true,
-        leave_dirs_open = true,
+      delete_to_trash = true,
+      watch_for_changes = true,
+      view_options = {
+        show_hidden = true,
+        -- This function defines what is considered a "hidden" file
+        is_hidden_file = function(name, bufnr)
+          local hidden = name:match("^%.") ~= nil
+          local env = name:match("^%.env(%..+)?$") ~= nil
+
+          return hidden and not env
+        end,
+        -- This function defines what will never be shown, even when `show_hidden` is set
+        is_always_hidden = function(name, bufnr)
+          local git = name:match("^%.git") ~= nil
+
+          return git
+        end,
+        natural_order = "fast",
+        case_insensitive = false,
+        sort = {
+          { "type", "asc" },
+          { "name", "asc" },
+        },
+        -- Customize the highlight group for the file name
+        -- FIXME: TOO SLOW, FIND BETTER APPROACH
+        -- highlight_filename = function(entry, is_hidden, is_link_target, is_link_orphan)
+        --   local output = vim.fn.system("git check-ignore " .. vim.fn.shellescape(entry.path))
+        --
+        --   if output ~= "" then
+        --     -- The name of the custom Oil git ignored highlight group
+        --     return "OilGitIgnored"
+        --   end
+        --
+        --   return nil
+        -- end,
       },
-      popup_border_style = 'rounded',
-      indent = {
-        indent_size = 4,
-        padding = 2,
-      },
-      window = {
-        position = 'float',
-      },
-      filesystem = {
-        hide_by_pattern = {
-          '*.meta',
-        },
-        always_show = {
-          '.gitignore',
-          '.env'
-        },
-        always_show_by_pattern = {
-          "./**/.env",
-          "./**/.env*",
-        },
-        never_show = {
-          '.DS_Store',
-          'thumbs.db',
-          '.idea',
-          '.vscode',
-        },
+      git = {
+        add = function(path)
+          return true
+        end,
+        mv = function(src_path, dest_path)
+          return true
+        end,
+        rm = function(path)
+          return true
+        end,
       },
     },
-  },
+  }
 }
