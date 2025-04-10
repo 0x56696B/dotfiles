e72cef1d047441af0db048c16805af16f711736c
Viktor Stamoff <transtrike@gmail.com>
2025-04-10 13:12:50 +0300

    feat(tmux): add named dir session in case of duplicate session names when invoking new dir session script

---
+++ roles/tmux/files/scripts/tmux-new-named-dir-session.sh
@@ -0,0 +1,35 @@
+#!/usr/bin/env bash
+
+CHOSEN_DIR=$(fd \
+  --type d \
+  --strip-cwd-prefix \
+  --hidden \
+  --follow \
+  --exclude .git \
+  --maxdepth 1 \
+  --full-path \
+  "$(pwd)" |
+  fzf --border \
+    --margin 1 \
+    --padding 1 \
+    --info inline \
+    --layout reverse \
+    --header $'Select new TMUX session working directory' \
+    --prompt 'Dir> ' \
+    --bind 'ctrl-p:up,ctrl-n:down,J:down,K:up' \
+    --tmux center)
+
+if [ -z "$CHOSEN_DIR" ]; then
+  exit 0
+fi
+
+# This it will append the PWD to the name of the chosen directory
+# Useful for when you want 2 dirs opened with the same name
+# Example: ./dotfiles/scripts => dotfiles-scripts
+CHOSEN_NAME="$(basename $(pwd))-${CHOSEN_DIR%/}"
+
+# Create the new session
+tmux new-session -d -s "$CHOSEN_NAME" -c "$(pwd)/${CHOSEN_DIR%/}"
+
+# Attach to the new session
+tmux switch-client -t "$CHOSEN_NAME"
