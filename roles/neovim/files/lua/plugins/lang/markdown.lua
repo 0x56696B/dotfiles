8604fa46de5950911d3df0d2a507ce311d884019
Viktor Stamoff <transtrike@gmail.com>
2025-07-07 13:50:51 +0300

    feat(markdown): add grouping for markdown in neovim

--- roles/neovim/files/lua/plugins/lang/markdown.lua
+++ roles/neovim/files/lua/plugins/lang/markdown.lua
@@ -10,6 +10,17 @@ return {
   { import = "lazyvim.plugins.extras.lang.markdown" },
 
   {
+    "folke/which-key.nvim",
+    opts = {
+      spec = {
+        {
+          { "<leader>m", group = "markdown" },
+        },
+      },
+    },
+  },
+
+  {
     "iamcco/markdown-preview.nvim",
     keys = {
       { "<leader>cp", false },
