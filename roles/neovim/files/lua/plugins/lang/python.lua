0a761a6938f402ab09d3c1f75dbc46015ab5e70d
Viktor Stamoff <transtrike@gmail.com>
2026-04-01 10:24:36 +0300

    fix(neovim): remove unneeded python config

--- roles/neovim/files/lua/plugins/lang/python.lua
+++ roles/neovim/files/lua/plugins/lang/python.lua
@@ -1,13 +1,6 @@
-local treesitter = { "python" }
-
+-- Use "basedpyright" Python LSP
 vim.g.lazyvim_python_lsp = "basedpyright"
 
 return {
   { import = "lazyvim.plugins.extras.lang.python" },
-
-  -- Syntax Highlighting
-  {
-    "nvim-treesitter/nvim-treesitter",
-    opts = { ensure_installed = treesitter },
-  },
 }
