b3c107dfd0ef4924ecd7759ce160729ef3da4166
Viktor Stamoff <transtrike@gmail.com>
2026-05-27 16:00:06 +0300

    feat(neovim): add debug keybinds like Visual Studio

--- roles/neovim/files/lua/plugins/base/debugging.lua
+++ roles/neovim/files/lua/plugins/base/debugging.lua
@@ -11,6 +11,42 @@ return {
         end,
         desc = "Inspect value under cursor",
       },
+
+      {
+        "<F5>",
+        function()
+          require("dap").continue()
+        end,
+        desc = "Debug: Continue",
+      },
+      {
+        "<F10>",
+        function()
+          require("dap").step_over()
+        end,
+        desc = "Debug: Step Over",
+      },
+      {
+        "<F11>",
+        function()
+          require("dap").step_into()
+        end,
+        desc = "Debug: Step Into",
+      },
+      {
+        "<S-F11>",
+        function()
+          require("dap").step_out()
+        end,
+        desc = "Debug: Step Out",
+      },
+      {
+        "<F9>",
+        function()
+          require("dap").toggle_breakpoint()
+        end,
+        desc = "Debug: Toggle Breakpoint",
+      },
     },
   },
 
