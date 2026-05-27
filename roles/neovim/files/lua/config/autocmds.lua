20b0c0fbb7a8d568fef8b9c4fc2d6729c343c47f
Viktor Stamoff <transtrike@gmail.com>
2026-05-27 15:59:07 +0300

    fix(neovim): add bang operator to Bd

--- roles/neovim/files/lua/config/autocmds.lua
+++ roles/neovim/files/lua/config/autocmds.lua
@@ -2,4 +2,6 @@
 -- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
 -- Add any additional autocmds here
 
-vim.api.nvim_create_user_command('Bd', 'bp | bd #', {})
+vim.api.nvim_create_user_command('Bd', function(args)
+  vim.cmd(args.bang and 'bp | bd! #' or 'bp | bd #')
+end, { bang = true })
